e3eff9490340e723af7c326f64922ea73238c9df
Randhawa Verma <randhawaverma@gmail.com>
2023-03-14 21:14:27 +0530

    Update logic to use text files instead of internal list datatype.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 1. Create empty list to store todos
-todo_list = []
+# todo_list = []
 
 while True:
     # Asking user choice to add, show, edit or exit
@@ -7,18 +7,25 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
-            todo_list.append(todo.title())
+            todo = input("Enter a todo: ") + "\n"
+            # todo_list.append(todo.title())
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+                todos.append(todo)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'show':
-            for i, item in enumerate(todo_list):
-                print(f"{i + 1}. {item}")
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+                for i, item in enumerate(todos):
+                    print(f"{i + 1}. {item}")
         case 'edit':
             number = int(input("Enter number of todo to edit: "))
             new_todo = input("Enter updated todo: ")
-            todo_list[number - 1] = new_todo
+            todos[number - 1] = new_todo
         case 'complete':
             number = int(input("Enter number of todo to edit: "))
-            todo_list.pop(number - 1)
+            todos.pop(number - 1)
         case 'exit':
             break
 
